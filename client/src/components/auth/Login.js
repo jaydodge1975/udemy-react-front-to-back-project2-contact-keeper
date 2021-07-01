e6d4e20c8dfe7adf76b2b3d19cf24d05654205f5
Jay Dodge <jaydodge1975@eastlink.ca>
2021-07-01 07:42:05 -0600

    Alert Context, State & Component /w alert message not showing warning flahes

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [user, serUser] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = user;
+
+  const onChange = e => serUser({ ...user, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.prevent.default();
+    console.log('Login submit');
+  };
+
+  return (
+    <div className='form-container'>
+      <h1>
+        Account <span className='text-secondary-bright'>Login</span>
+      </h1>
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
+          <label htmlFor='email'>Email Address</label>
+          <input
+            type='email'
+            name='email'
+            value={email}
+            onChange={onChange}
+            autoComplete='off'
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            name='password'
+            value={password}
+            onChange={onChange}
+            autoComplete='off'
+          />
+        </div>
+        <input
+          type='submit'
+          value='Login'
+          className='btn btn-primary btn-block'
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
